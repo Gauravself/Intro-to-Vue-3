24ab9243722917ede8b9122a235da2b121bc9995
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-07-23 10:43:51 +0530

    Attribute Bnding

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+//Create vue app
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description:
+        "Best Comfortable socks for daily use, made using pure cotton can be used in both summer and winters",
+    };
+  },
+});
